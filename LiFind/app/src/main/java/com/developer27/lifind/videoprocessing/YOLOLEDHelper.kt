7ed7539c44d76a7e0e500a82b0be702423ebdf31
Deniz K Acikbas <denizkarya1999@outlook.com>
2025-09-28 21:59:46 -0400

    <DKA - September 28th 2025> YOLO 11M (Ultralytics Hub trained) has been selected for current distance dataset.

--- LiFind/app/src/main/java/com/developer27/lifind/videoprocessing/YOLOLEDHelper.kt
+++ LiFind/app/src/main/java/com/developer27/lifind/videoprocessing/YOLOLEDHelper.kt
@@ -17,7 +17,7 @@ import java.nio.channels.FileChannel
 object YOLOLEDHelper {
 
     const val INPUT_SIZE = 640
-    private const val MODEL_ASSET_NAME = "lifind_iso_led_100_yolo_11m_google_collab.tflite"
+    private const val MODEL_ASSET_NAME = "lifind_iso_led_100_yolo_11m_ultralytics_hub.tflite"
 
     @Volatile
     private var interpreter: Interpreter? = null
